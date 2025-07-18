534d59917123630148475b29613655233f21264f
Matloga <tsumbedzomatloga@gmail.com>
2025-07-18 10:30:25 +0200

    Comments

---
+++ 1_Java_Fundamentals/01_Variables_and_Operations/Comments_and_Syntax/01_Hello_World/Comments.java
@@ -0,0 +1,14 @@
+public class Comments 
+{
+    public static void main(String[] T_man)
+    {
+        // This is a single line comment
+        System.out.println("Hello, Mr Matloga Sir here."); // This message will appear on the console.
+        /*
+         This is a multi line comment.
+         It can be used in multiple lines.
+         Mostly is used to explain something in details.
+         */
+        System.out.println("I am on my way to become Java full stack developer"); // Here I am just sharing my interest.
+    }
+}
