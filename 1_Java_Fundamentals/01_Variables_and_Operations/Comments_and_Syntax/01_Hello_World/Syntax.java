672a321312243ecd82419e6cc3211603ec599adf
Matloga <tsumbedzomatloga@gmail.com>
2025-07-18 10:48:20 +0200

    Syntax

---
+++ 1_Java_Fundamentals/01_Variables_and_Operations/Comments_and_Syntax/01_Hello_World/Syntax.java
@@ -0,0 +1,8 @@
+public class Syntax 
+{
+    public static void main(String[] args)
+    {
+        // Here I want to write the Java Syntax
+        System.out.println("This is the Java Syntax");
+    }
+}
