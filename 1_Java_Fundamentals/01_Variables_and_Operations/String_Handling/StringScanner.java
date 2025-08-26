979a03e07c94b4edca75f064a9260b5edbf226ab
Matloga <tsumbedzomatloga@gmail.com>
2025-08-26 15:01:24 +0200

    T-man

--- 1_Java_Fundamentals/01_Variables_and_Operations/String_Handling/StringScanner.java
+++ 1_Java_Fundamentals/01_Variables_and_Operations/String_Handling/StringScanner.java
@@ -12,7 +12,18 @@ public class StringScanner
         s1 = scanner.nextLine();
         s2 = scanner.nextLine();
 
+        int 11 = s1.length();
+        int 12 = s2.length();
+
+        System.out.println("Length of first string is : " + 11);
+        System.out.println("Length of second string is : " + 12);
+
         String s3 = s1 + s2;   // s1.concat(s2);
         System.out.println("Results on Concatination is : " + s3);
+
+        s1 = s1.toUpperCase();
+        System.out.println("First string in Upper Case : " + s1);
+
+        s2 = s2.toLowerCase();
     }  
 }
