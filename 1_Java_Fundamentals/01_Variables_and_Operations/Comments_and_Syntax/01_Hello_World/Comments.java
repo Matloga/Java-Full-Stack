af48d3067c2238a75a1e452ef64c9da3c340164b
Matloga <tsumbedzomatloga@gmail.com>
2025-08-26 13:50:03 +0200

    T-man

--- 1_Java_Fundamentals/01_Variables_and_Operations/Comments_and_Syntax/01_Hello_World/Comments.java
+++ 1_Java_Fundamentals/01_Variables_and_Operations/Comments_and_Syntax/01_Hello_World/Comments.java
@@ -3,11 +3,11 @@ public class Comments
     public static void main(String[] T_man)
     {
         // This is a single line comment
-        System.out.println("Hello, Mr Matloga Sir here."); // This message will appear on the console.
+        System.out.println("Hello, Mr Matloga Sir here."); // When I run this program, it will print the text in the console.
         /*
          This is a multi-line comment.
-         It can be used in multiple lines.
-         Mostly is used to explain something in details.
+         we cwn used it in multiple lines.
+         We can use it if we want to explain something in details.
          */
         System.out.println("I am on my way to become Java full stack developer"); // Here I am just sharing my interest.
     }
